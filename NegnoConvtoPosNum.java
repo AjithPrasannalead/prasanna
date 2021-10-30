246f3135203c6335da8c861eaa355c2330ef459e
AjithPrasannalead <93427588+AjithPrasannalead@users.noreply.github.com>
2021-10-31 00:54:20 +0530

    Add files via upload

---
+++ NegnoConvtoPosNum.java
@@ -0,0 +1,22 @@
+package Ajithday01;
+
+public class NegnoConvtoPosNum {
+
+	public static void main(String[] args) {
+
+int number = -40;
+		if (number <0) {
+			System.out.println("Number is negative");
+			
+			number = number * -1; 
+			System.out.println("The number is converted to negative to positive");
+		}
+			else if (number>0) {
+				System.out.println("The number is positive");
+			}		
+	} 
+		
+
+	}
+
+
---
+++ TheNumisPosorNeg.java
@@ -0,0 +1,28 @@
+package Ajithday01;
+
+public class TheNumisPosorNeg {
+
+	public static void main(String[] args) {
+
+		
+int number = 35;
+		
+		if (number>0) {
+			System.out.println("Number is positive");     // This condition always true 
+		}
+			
+			else if (number < 0) {
+				System.out.println("Number is negative");  // This condition always false
+				
+			}
+		
+			else {
+				System.out.println("The number is neither positive nor negative"); 
+				
+			
+			
+		}
+
+	}
+
+}
